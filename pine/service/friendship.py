09f4fee6a77e1a2c46196dbffb1e8ff14b589075
reaperes <emerald105@hanmail.net>
2014-08-03 22:26:38 +0900

    Hanyong phone bug fixed.

--- pine/service/friendship.py
+++ pine/service/friendship.py
@@ -17,10 +17,10 @@ def create_friendship(user, target_phone_number):
 
     # add target phone to friends phones
     try:
-        print(str(user))
-        print(str(target_phone))
         user.friend_phones.add(target_phone)
     except Exception as err:
+        print(str(user))
+        print(str(target_phone))
         print(str(err))
         print('here?')
         raise err
