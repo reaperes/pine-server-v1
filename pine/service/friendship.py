fd2c484b7959ec7d5422336a08ba3ce0a5b00d12
reaperes <emerald105@hanmail.net>
2014-08-03 22:11:38 +0900

    Hanyong phone bug fixed.

--- pine/service/friendship.py
+++ pine/service/friendship.py
@@ -2,49 +2,34 @@ from pine.models import Users, Phones
 
 
 def create_friendship(user, target_phone_number):
-    test = None
-    try:
-        test = 1
-        # get or create target phone record
-        target_phone_id = Phones.objects.filter(phone_number=target_phone_number)
-        target_phone = None
-        if target_phone_id.exists():
-            target_phone = Phones.objects.get(phone_number=target_phone_number)
-        else:
-            target_phone = Phones.objects.create(phone_number=target_phone_number)
-        test = 2
-        # check for performance.
-        # If does not need to compute it return
-        if user.friend_phones.filter(phone_number=target_phone_number).exists():
-            return
-        test = 3
-        # add target phone to friends phones
-        try:
-            user.friend_phones.add(target_phone)
-        except Exception as err:
-            print('hu????!!!!!!!')
-            print(str(err))
-            raise err
-        test = 3.1
-        # check for performance.
-        # If target is no pine user it return
-        target_user_query = Users.objects.filter(phone=target_phone)
-        test = 3.2
-        target_user = None
-        if target_user_query.exists():
-            test = 3.21
-            target_user = Users.objects.get(phone=target_phone)
-        else:
-            test = 3.22
-            return
-        test = 4
-        # configure friends, following
-        has_user_target_phone = target_user.friend_phones.filter(id=user.id).exists()
-        if has_user_target_phone:
-            user.friends.add(target_user)
-        else:
-            user.followings.add(target_user)
+    # get or create target phone record
+    target_phone_id = Phones.objects.filter(phone_number=target_phone_number)
+    target_phone = None
+    if target_phone_id.exists():
+        target_phone = Phones.objects.get(phone_number=target_phone_number)
+    else:
+        target_phone = Phones.objects.create(phone_number=target_phone_number)
 
-    except Exception as err:
-        print(test)
-        raise err
+    # check for performance.
+    # If does not need to compute it return
+    if user.friend_phones.filter(id=target_phone.id).exists():
+        return
+
+    # add target phone to friends phones
+    user.friend_phones.add(target_phone)
+
+    # check for performance.
+    # If target is no pine user it return
+    target_user_query = Users.objects.filter(id=target_phone.id)
+    target_user = None
+    if target_user_query.exists():
+        target_user = Users.objects.get(id=target_phone.id)
+    else:
+        return
+
+    # configure friends, following
+    has_user_target_phone = target_user.friend_phones.filter(id=user.id).exists()
+    if has_user_target_phone:
+        user.friends.add(target_user)
+    else:
+        user.followings.add(target_user)
