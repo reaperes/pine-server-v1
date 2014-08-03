9921568a0c2f350ad28d8d2296df94d164c46d29
reaperes <emerald105@hanmail.net>
2014-08-03 21:48:17 +0900

    for test

--- pine/service/friendship.py
+++ pine/service/friendship.py
@@ -2,6 +2,7 @@ from pine.models import Users, Phones
 
 
 def create_friendship(user, target_phone_number):
+    print(1)
     # get or create target phone record
     target_phone_id = Phones.objects.filter(phone_number=target_phone_number)
     target_phone = None
@@ -9,12 +10,12 @@ def create_friendship(user, target_phone_number):
         target_phone = Phones.objects.get(phone_number=target_phone_number)
     else:
         target_phone = Phones.objects.create(phone_number=target_phone_number)
-
+    print(2)
     # check for performance.
     # If does not need to compute it return
     if user.friend_phones.filter(phone_number=target_phone_number).exists():
         return
-
+    print(3)
     # add target phone to friends phones
     user.friend_phones.add(target_phone)
 
@@ -26,7 +27,7 @@ def create_friendship(user, target_phone_number):
         target_user = target_user_query[0]
     else:
         return
-
+    print(4)
     # configure friends, following
     has_user_target_phone = target_user.friend_phones.filter(id=user.id).exists()
     if has_user_target_phone:
